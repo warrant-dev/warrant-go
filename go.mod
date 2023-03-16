d334e2229c8b58a6e5e36b8679c7c8d945e7c9be
Stanley Phu <stanley@warrant.dev>
2023-03-15 22:12:49 -0700

    Add v3 module (#25)

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/warrant-dev/warrant-go/v2
+module github.com/warrant-dev/warrant-go/v3
 
 go 1.19
 
--- user/client.go
+++ user/client.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 
 	"github.com/google/go-querystring/query"
-	"github.com/warrant-dev/warrant-go/v2"
-	"github.com/warrant-dev/warrant-go/v2/client"
-	"github.com/warrant-dev/warrant-go/v2/config"
+	"github.com/warrant-dev/warrant-go/v3"
+	"github.com/warrant-dev/warrant-go/v3/client"
+	"github.com/warrant-dev/warrant-go/v3/config"
 )
 
 type Client struct {
