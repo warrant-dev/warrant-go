ebbfdcefdcf5644b0556cafd205a7195221f7f12
stanleyphu <phu.stanley@gmail.com>
2023-08-24 13:18:32 -0700

    Simplify request logic

--- apiclient.go
+++ apiclient.go
@@ -30,31 +30,16 @@ func NewApiClient(config ClientConfig) *ApiClient {
 }
 
 func (client ApiClient) MakeRequest(method string, path string, payload interface{}, options *RequestOptions) (*http.Response, error) {
-	url := client.Config.ApiEndpoint + path
-	if payload == nil {
-		req, err := http.NewRequest(method, url, nil)
+	var requestBody *bytes.Buffer
+	if payload != nil {
+		postBody, err := json.Marshal(payload)
 		if err != nil {
-			return nil, WrapError("Unable to create request", err)
+			return nil, WrapError("Invalid request payload", err)
 		}
-		if client.Config.ApiKey != "" {
-			req.Header.Add("Authorization", fmt.Sprintf("ApiKey %s", client.Config.ApiKey))
-		}
-		if options.WarrantToken != "" {
-			req.Header.Add("Warrant-Token", options.WarrantToken)
-		}
-		req.Header.Add("User-Agent", fmt.Sprintf("warrant-go/%s", ClientVersion))
-		resp, err := client.HttpClient.Do(req)
-		if err != nil {
-			return nil, WrapError("Error making request", err)
-		}
-		return resp, nil
+		requestBody = bytes.NewBuffer(postBody)
 	}
 
-	postBody, err := json.Marshal(payload)
-	if err != nil {
-		return nil, WrapError("Invalid request payload", err)
-	}
-	requestBody := bytes.NewBuffer(postBody)
+	url := client.Config.ApiEndpoint + path
 	req, err := http.NewRequest(method, url, requestBody)
 	if err != nil {
 		return nil, WrapError("Unable to create request", err)
@@ -66,11 +51,11 @@ func (client ApiClient) MakeRequest(method string, path string, payload interfac
 		req.Header.Add("Warrant-Token", options.WarrantToken)
 	}
 	req.Header.Add("User-Agent", fmt.Sprintf("warrant-go/%s", ClientVersion))
+
 	resp, err := client.HttpClient.Do(req)
 	if err != nil {
 		return nil, WrapError("Error making request", err)
 	}
-
 	respStatus := resp.StatusCode
 	if respStatus < 200 || respStatus >= 400 {
 		msg, _ := io.ReadAll(resp.Body)
